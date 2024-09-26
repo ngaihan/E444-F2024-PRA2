1d61259e444aba066df837a3d16425821774c72d
Ryan Ngai <ryanngai21@gmail.com>
2024-09-26 19:28:08 -0400

    added the top bar

--- hello.py
+++ hello.py
@@ -1,17 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
-#@app.route('/user/<name>')
-#def user(name):
-#    return '<h1>Hello, {name}!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
-def hello_name(name):
-    return f"<h1>Hello {name}!</h1>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def user(name):
+    return render_template('user.html', name=name)
