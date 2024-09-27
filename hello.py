cb6156d7754a4cb7e6487ae8a391256faef4c1b7
Ryan Ngai <ryanngai21@gmail.com>
2024-09-26 23:20:42 -0400

    Implemented email and email verification

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, ValidationError
+import email_validator
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,7 +15,12 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('Enter your email:', validators=[DataRequired(), Email()])  # New email field
     submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if not field.data.endswith('@mail.utoronto.ca'):
+            raise ValidationError('Email must end with @mail.utoronto.ca') 
 
 
 @app.errorhandler(404)
@@ -32,8 +38,12 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data  # Store email in session
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
